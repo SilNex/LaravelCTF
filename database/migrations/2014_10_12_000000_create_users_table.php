ea90fb0e177ee74867140941641a2ff56d808763
silnex <silnex@silnex.kr>
2019-02-19 19:02:04 +0900

    Add privilege column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('score')->default(0);
+            $table->integer('privilege')->default(-1);
             $table->rememberToken();
             $table->timestamps();
         });
