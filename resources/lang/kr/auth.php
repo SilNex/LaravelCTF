392974ae8a34ebedad6ea9a6b097877ac800cbcb
silnex <silnex@silnex.kr>
2019-03-07 17:47:04 +0900

    Change FormRequest custom error message to resource/lang

--- resources/lang/kr/auth.php
+++ resources/lang/kr/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '아이디 또는 비밀번호를 다시 확인하세요.',
+    'throttle' => '로그인 시도 횟수가 너무 많습니다. :seconds 초 후에 다시 시도해 주세요',
 
 ];
--- resources/lang/kr/passwords.php
+++ resources/lang/kr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    "password" => "비밀번호는 최소한 6자리 이상이며 일치해야 합니다.",
+	"reset" => "비밀번호를 초기화하였습니다.",
+	"sent" => "비밀번호 초기화 링크를 이메일로 보내드렸습니다.",
+	"token" => "이 비밀번호 초기화 토큰은 유효하지 않습니다.",
+	"user" => "이 이메일을 사용하는 사용자를 찾을 수 없습니다.",
 
 ];
