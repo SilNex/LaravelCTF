cc94f49487e2d06e7b3e09784d006713169559d9
silnex <silnex@silnex.kr>
2019-03-25 01:32:30 +0900

    Add main controller

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::apiResource('challenges', 'ChallengeController');
 
 Route::post('/challenges/{challenge}', 'FlagAuthentication@checkFlag')->name('challenges.check_flag');
+
+Route::get('/', 'MainController@index')->name('main');
